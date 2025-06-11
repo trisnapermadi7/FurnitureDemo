1ce453dbf87f95700d24919b34e1878f1718c235
QOHARY <hari.hujan71@gmail.com>
2025-06-11 16:37:28 +0800

    Furniturecard ArView

--- src/components/FurnitureCard.js
+++ src/components/FurnitureCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './FurnitureCard.css';
 
-function FurnitureCard({ name, price, image, onClick }) {
+function FurnitureCard({ name, image, onClick }) {
   return (
-    <div className="furniture-card" onClick={onClick}>
+    <div className="furniture-card">
       <img src={image} alt={name} className="card-image" />
       <div className="card-info">
         <h3 className="card-title">{name}</h3>
-        <p className="card-price">${price}</p>
+        <button className="card-button" onClick={onClick}>
+          Lihat Tampilan
+        </button>
       </div>
     </div>
   );
